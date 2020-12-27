1b88c684776596f57196144494e49cd338568dff
Marcin Krawczyk <marcin.krawczyk@ef.com>
2020-12-28 00:38:36 +0100

    [add] header bar with some placeholders

---
+++ components/Header/index.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <div className="bg-blue-100 max-w-full pb-2 pt-2">
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h1 className="uppercase">task managment app</h1>
+                    </div>
+                    <div>
+                        <div className="bg-red-200 h-6 w-6" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
