0e9467b7ff62000daef587eb7f21c6c1fe3d324b
Marcin Krawczyk <marcin.krawczyk@ef.com>
2020-12-28 01:20:00 +0100

    [add] task context for managing task actions

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,15 @@
+import {useContext} from "react";
 import Adjustments from "../Icons/Adjustments";
 import AdjustmentsOutline from "../Icons/AdjustmentsOutline";
 import Search from "../Icons/Search";
 import SearchOutline from "../Icons/SearchOutline";
 import PlusOutline from "../Icons/PlusOutline";
 import Plus from "../Icons/Plus";
+import {TaskContext} from "../../contexts/TaskContext";
 
 const Header = () => {
+    const {actions} = useContext(TaskContext)
+
     return (
         <div className="bg-blue-100 max-w-full pb-2 pt-2">
             <div className="container mx-auto">
@@ -14,7 +18,7 @@ const Header = () => {
                         <h1 className="uppercase">task managment app</h1>
                     </div>
                     <div className="flex">
-                        <PlusOutline className="w-6 h-6" />
+                        <PlusOutline className="w-6 h-6" onClick={actions.addTask} />
                         <Plus className="w-6 h-6" />
                         <Search className="w-6 h-6" />
                         <SearchOutline className="w-6 h-6" />
--- contexts/index.js
+++ contexts/index.js
@@ -1,7 +1,9 @@
+import TaskContextProvider from "./TaskContext";
+
 const ContextWrapper = ({children}) => (
-    <>
+    <TaskContextProvider>
       {children}
-    </>
+    </TaskContextProvider>
 )
 
 export default ContextWrapper;
